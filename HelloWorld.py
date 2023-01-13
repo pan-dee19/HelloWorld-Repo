004c7f06a1c0bc9343e5b189b93a6469e82b4347
pan-dee19 <99569891+pan-dee19@users.noreply.github.com>
2023-01-13 13:39:01 -0600

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,9 @@
+# My first Python Program
+
+print ("Hello World!")
+First_Name = "Ruth"
+Last_Name = "Mechegia"
+Full_Name = First_Name + " " + Last_Name
+
+print("Hi Every one, my name is " + Full_Name + " and I am currently taking a python course")
+
